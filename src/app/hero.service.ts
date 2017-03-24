afd127fb643ae70ac29ba2be071e76044b8610c1
Clément LE FAY <clement.lefay@sodexo.com>
2017-03-24 17:25:52 +0100

    Fifth part of the Angular training - routing details

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+
+
 @Injectable()
+
+
 export class HeroService {
+    getHero(id: number){
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
+    }
     getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
     }
